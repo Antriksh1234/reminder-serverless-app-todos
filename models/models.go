dfe4c2060245ff99418086c8ad571995ff62ba26
Antriksh1234 <antrikshtelang1998@gmail.com>
2023-04-09 10:18:13 +0530

    Change Todo model

--- models/models.go
+++ models/models.go
@@ -4,6 +4,7 @@ import "time"
 
 type Todo struct {
 	ID          string    `json:id`
+	UserID      string    `json:userID`
 	Title       string    `json:title`
 	Description string    `json:description`
 	Deadline    time.Time `json:deadline`
